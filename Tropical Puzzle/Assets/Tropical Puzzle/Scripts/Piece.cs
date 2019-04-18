10af3d9172d05d4459b97343a6181f1ff47c0794
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-18 08:47:06 -0500

    Implementation Swipe for pieces

--- Tropical Puzzle/Assets/Tropical Puzzle/Scripts/Piece.cs
+++ Tropical Puzzle/Assets/Tropical Puzzle/Scripts/Piece.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
@@ -6,7 +7,7 @@ public class Piece : MonoBehaviour
     private static Piece _previousSelected = null;
 
     private SpriteRenderer _spriteRenderer;
-    private bool isSelected = false;
+    private bool _isSelected = false;
 
     private Vector2[] _adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.right, Vector2.left };
 
@@ -15,5 +16,102 @@ public class Piece : MonoBehaviour
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void SelectPiece()
+    {
+        _isSelected = true;
+        _spriteRenderer.color = _selectedColor;
+        _previousSelected = gameObject.GetComponent<Piece>();
+    }
+
+    private void DeselectPiece()
+    {
+        _isSelected = false;
+        _spriteRenderer.color = Color.white;
+        _previousSelected = null;
+    }
+
+    private void OnMouseDown()
+    {
+        if (_spriteRenderer == null || GridManager.Instance.IsShifting)
+        {
+            return;
+        }
+
+        if (!_isSelected)
+        {
+            if (_previousSelected == null)
+            {
+                SelectPiece();
+            }
+            else
+            {
+                if (CanSwipe())
+                {
+                    SwapSprite(_previousSelected);
+                    _previousSelected.DeselectPiece();
+                }
+                else
+                {
+                    _previousSelected.DeselectPiece();
+                    SelectPiece();
+                }
+            }
+        }
+        else
+        {
+            DeselectPiece();
+        }
+    }
+
+    public void SwapSprite(Piece newPiece)
+    {
+        if (_spriteRenderer.sprite == newPiece.GetComponent<SpriteRenderer>().sprite)
+        {
+            return;
+        }
+
+        Sprite oldPiece = newPiece._spriteRenderer.sprite;
+        newPiece._spriteRenderer.sprite = this._spriteRenderer.sprite;
+        this._spriteRenderer.sprite = oldPiece;
+
+        int tmpID = newPiece.iD;
+        newPiece.iD = this.iD;
+        this.iD = tmpID;
+    }
+
+    private GameObject GetNeighbor(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position,
+                                         direction);
+
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+
+        }
+        else
+        {
+            return null;
+
+        }
+    }
+
+    private List<GameObject> GetAllNeighbors()
+    {
+        List<GameObject> neighbors = new List<GameObject>();
+
+        foreach (Vector2 direction in _adjacentDirections)
+        {
+            neighbors.Add(GetNeighbor(direction));
+        }
+
+        return neighbors;
+    }
+
+    private bool CanSwipe()
+    {
+        return GetAllNeighbors().Contains(_previousSelected.gameObject);
     }
 }
