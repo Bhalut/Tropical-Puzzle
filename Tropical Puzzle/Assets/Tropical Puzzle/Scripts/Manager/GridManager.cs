d6b53204625a9ea447e8cfe2f3e75f0918e6c066
Abdel Mejia <bhalut.bear@gmail.com>
2019-04-21 11:26:10 -0500

    Implemented FindNullPieces

--- Tropical Puzzle/Assets/Tropical Puzzle/Scripts/Manager/GridManager.cs
+++ Tropical Puzzle/Assets/Tropical Puzzle/Scripts/Manager/GridManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -41,14 +42,13 @@ public class GridManager : MonoBehaviour
 
         float startX = this.transform.position.x;
         float startY = this.transform.position.y;
-        int idx = -1;
         for (int x = 0; x < xSize; x++)
         {
             for (int y = 0; y < ySize; y++)
             {
                 GameObject newPiece = Instantiate(currentPiece, new Vector2(startX + (offset.x * x), startY + (offset.y * y)), currentPiece.transform.rotation);
                 newPiece.name = $"Piece[{x}][{y}]";
-
+                int idx;
                 do
                 {
                     idx = Random.Range(0, _prefabs.Count);
@@ -61,5 +61,51 @@ public class GridManager : MonoBehaviour
                 _pieces[x, y] = newPiece;
             }
         }
+    }
+
+    public IEnumerator FindNullPieces()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                if (_pieces[x, y].GetComponent<SpriteRenderer>().sprite == null)
+                {
+                    yield return StartCoroutine(MakePiecesFall(x, y));
+                    break;
+                }
+            }
+        }
+    }
+
+    private IEnumerator MakePiecesFall(int x, int yStart, float shiftDelay = 0.05f)
+    {
+        IsShifting = true;
+
+        List<SpriteRenderer> renderers = new List<SpriteRenderer>();
+
+        int nullPieces = 0;
+
+        for (int y = yStart; y < ySize; y++)
+        {
+            SpriteRenderer spriteRenderer = _pieces[x, y].GetComponent<SpriteRenderer>();
+            if (spriteRenderer.sprite == null)
+            {
+                nullPieces++;
+            }
+            renderers.Add(spriteRenderer);
+        }
+
+        for (int i = 0; i < nullPieces; i++)
+        {
+            yield return new WaitForSeconds(shiftDelay);
+            for (int j = 0; j < renderers.Count - 1; j++)
+            {
+                renderers[j].sprite = renderers[j + 1].sprite;
+                renderers[j + 1].sprite = null;
+            }
+        }
+
+        IsShifting = false;
     }
 }
